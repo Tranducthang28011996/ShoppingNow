52f6718cbb892018fa14ab67b7b034f3ef5ed481
Tranducthang28011996 <tieuhodo96@gmail.com>
2017-07-25 16:49:30 +0700

    config detail product

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -3,4 +3,8 @@ class ProductController < ApplicationController
     @products =  Category.find(params[:id].to_i).products
     @order_item = current_order.order_items.new
   end
+
+  def show
+    @product =  Product.find_by_id params[:id]
+  end
 end
